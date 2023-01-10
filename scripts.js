1d63314cc81cf370aa1bdacf54520a7fc6e65eed
madmaxmatze <madmaxmatze@gmail.com>
2023-01-10 21:37:35 +0000

    bug fix

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-var writingDiv = document.getElementById("writing");
-if (writingDiv) {
+var writingElement = document.getElementById("writing");
+if (writingElement) {
     (async () => {
         var query = `{
             user(username: "madmaxmatze") {
@@ -23,28 +23,30 @@ if (writingDiv) {
         })
             .then(response => response.json())
             .then(json => {
-                writingDiv.innerHTML = json.data.user.publication.posts.slice(0, 2).map(post => (
+                writingElement.innerHTML = json.data.user.publication.posts.slice(0, 2).map(post => (
                     `<li><a href="https://${json.data.user.publicationDomain}/${post.slug}" target="_blank">
                         ${post.title} (${(new Date(post.dateAdded)).toLocaleDateString(undefined, { month: 'short', day: 'numeric' })})
                     </a></li>`
-                )).join("") + writingDiv.innerHTML;
+                )).join("") + writingElement.innerHTML;
             });
     })();
 }
 
-
-new Typed(".typed", { // https://github.com/mattboldt/typed.js/
-    strings: [
-        "I'm a coder"
-        , "I'm a proud father of two"
-        , "Ich bin ein Berliner"
-        , "I build tech teams and products @work"
-        , "I practice Muay-Thai"
-        , "I'm " + new Date(Date.now() - 62585735726663).getFullYear() + " years old"
-        , "I run this site since 2002"
-    ].sort(() => .5 - Math.random())
-    , typeSpeed: 90
-    , backDelay: 700
-    , loop: true
-    //,shuffle: true // <- buggy
-});
+var typedElement = document.getElementById("typed");
+if (typedElement) {
+    new Typed(typedElement, { // https://github.com/mattboldt/typed.js/
+        strings: [
+            "I'm a coder"
+            , "I'm a proud father of two"
+            , "Ich bin ein Berliner"
+            , "I build tech teams and products @work"
+            , "I practice Muay-Thai"
+            , "I'm " + new Date(Date.now() - 62585735726663).getFullYear() + " years old"
+            , "I run this site since 2002"
+        ].sort(() => .5 - Math.random())
+        , typeSpeed: 90
+        , backDelay: 700
+        , loop: true
+        //,shuffle: true // <- buggy
+    });
+}
