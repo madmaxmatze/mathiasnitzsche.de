d16c949106e0026768c5aab7a232a3e8923b6b2c
madmaxmatze <madmaxmatze@gmail.com>
2023-01-10 20:56:49 +0000

    improvement

--- scripts.js
+++ scripts.js
@@ -1,51 +1,50 @@
-if (document.getElementById("writing")) {
-  (async () => {
-    var query = `{
-        user(username: "madmaxmatze") {
-        blogHandle
-        publicationDomain
-        publication {
-            posts(page: 0) {
-            dateAdded
-            slug
-            title
-            brief
-            coverImage
+var writingDiv = document.getElementById("writing");
+if (writingDiv) {
+    (async () => {
+        var query = `{
+            user(username: "madmaxmatze") {
+                blogHandle
+                publicationDomain
+                publication {
+                    posts(page: 0) {
+                    dateAdded
+                    slug
+                    title
+                    brief
+                    coverImage
+                    }
+                }
             }
-        }
-        }
-    }`;
-    fetch('https://api.hashnode.com/', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({query})
-    })
-    .then(response => response.json())
-    .then(json => {
-        document.getElementById("writing").innerHTML = json.data.user.publication.posts.slice(0, 2).map(post => (
-        `<li><a href="https://${json.data.user.publicationDomain}/${post.slug}" target="_blank">
-            ${post.title} (${ (new Date (post.dateAdded)).toLocaleDateString(undefined, {month: 'short', day: 'numeric'})})
-        </a></li>`
-        )).join("") + document.getElementById("writing").innerHTML;
-    });
-  })();
+        }`;
+        fetch('https://api.hashnode.com/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query })
+        })
+            .then(response => response.json())
+            .then(json => {
+                writingDiv.innerHTML = json.data.user.publication.posts.slice(0, 2).map(post => (
+                    `<li><a href="https://${json.data.user.publicationDomain}/${post.slug}" target="_blank">
+                        ${post.title} (${(new Date(post.dateAdded)).toLocaleDateString(undefined, { month: 'short', day: 'numeric' })})
+                    </a></li>`
+                )).join("") + writingDiv.innerHTML;
+            });
+    })();
 }
-    
 
-if (document.getElementsByClassName("typed")) {
-  new Typed(".typed", { // https://github.com/mattboldt/typed.js/
+
+new Typed(".typed", { // https://github.com/mattboldt/typed.js/
     strings: [
         "I'm a coder"
-        ,"I'm a proud father of two"
-        ,"Ich bin ein Berliner"
-        ,"I build tech teams and products @work"
-        ,"I practice Muay-Thai"
-        ,"I'm " + new Date(Date.now()-62585735726663).getFullYear() + " years old"
-        ,"I run this site since 2002"
+        , "I'm a proud father of two"
+        , "Ich bin ein Berliner"
+        , "I build tech teams and products @work"
+        , "I practice Muay-Thai"
+        , "I'm " + new Date(Date.now() - 62585735726663).getFullYear() + " years old"
+        , "I run this site since 2002"
     ].sort(() => .5 - Math.random())
-    ,typeSpeed: 90
-    ,backDelay: 700
-    ,loop: true
+    , typeSpeed: 90
+    , backDelay: 700
+    , loop: true
     //,shuffle: true // <- buggy
-  });
-}
+});
